ab252d1a0f1987985cc649aa32ea69b81a3281e7
ryaniero <hid.iero@gmail.com>
2026-06-11 06:20:48 -0400

    Fix pylint issues, achieve 10/10 score

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Flask application for Emotion Detection using Watson NLP."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +6,7 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """Analyze the emotion of the given text and return the result."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -23,8 +25,8 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+    """Render the index page."""
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
